66da55c4a2aa48ae00ea3ebfd399fbf03412b2cf
Alex <wraithmaster10@protonmail.com>
2021-07-06 17:09:08 +1200

    wallah

---
+++ config.js
@@ -0,0 +1,8 @@
+const assetTicker = "iRX";
+const logoSrc = "eg-logo.svg";
+const primaryColor = "#6C63FF";   
+const secondaryColor = "#3AD29F";
+const thirdColor = "#F55F44";
+// trophy colors top -> bottom
+const trophyColors = ["#C3C3C3","#F4B226","#C2907D"];
+const trophyTiers = ["platinum", "gold", "bronze"];
