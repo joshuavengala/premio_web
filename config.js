8de5d3afcd1e3c6e1fe074cd31470f366b468e2d
joshuavengala <87107700+joshuavengala@users.noreply.github.com>
2021-07-08 15:31:42 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const assetTicker = "iRX";
 const logoSrc = "https://upload.wikimedia.org/wikipedia/sco/thumb/b/bf/KFC_logo.svg/1200px-KFC_logo.svg.png";
-const primaryColor = "#6C63FF";   
-const secondaryColor = "#3AD29F";
-const thirdColor = "#F55F44";
+const primaryColor = "#A3080C";   
+const secondaryColor = "#F5D4B7";
+const thirdColor = "#FFF1E2";
 // trophy colors top -> bottom
 const trophyColors = ["#C3C3C3","#F4B226","#C2907D"];
 const trophyTiers = ["platinum", "gold", "bronze"];
