0ca8cc589f6c5ecbddce2cf052dc4f91aa11e943
4855062503 <72790952+4855062503@users.noreply.github.com>
2021-07-08 15:17:17 +1200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const assetTicker = "iRX";
-const logoSrc = "eg-logo.svg";
+const logoSrc = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/546px-McDonald%27s_Golden_Arches.svg.png";
 const primaryColor = "#6C63FF";   
 const secondaryColor = "#3AD29F";
 const thirdColor = "#F55F44";
